dc78df0ff7956dd024ca2e58c2366e9aec81a62f
ThiagoMeseguer <129554121+ThiagoMeseguer@users.noreply.github.com>
2023-04-14 22:03:40 -0300

    Add files via upload

---
+++ TP1/js/funciones.js
@@ -0,0 +1,9 @@
+console.log ("Funciona")
+//const aux=document.getElementById(cajatexto);
+
+function mifuncion(){
+const aux=document.getElementById("apellido");
+const aux2=document.getElementById("nombre");
+const aux3=document.getElementById("edad");
+alert(aux.value+" "+aux2.value+" "+aux3.value);
+}
