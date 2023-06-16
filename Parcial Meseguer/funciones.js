de5654c29cb5e9c6fd0a08f8f9e71580f51a23d4
ThiagoMeseguer <129554121+ThiagoMeseguer@users.noreply.github.com>
2023-06-16 19:22:06 -0300

    Add files via upload

---
+++ Parcial Meseguer/funciones.js
@@ -0,0 +1,26 @@
+function getBuscar() {
+    const idmuni=document.getElementById("id-municipio").value
+    const idprov=document.getElementById("id-provincia").value
+    let contenedor=document.getElementById("contenedor")
+    fetch("asentamientos.json")
+    .then(res=>res.json())
+    .then(function(data){
+        console.log(data)
+        
+        let localidades=data.localidades
+        for (let i = 0; i < localidades.length; i++) {
+            if ((idmuni>0)&&(idprov>0)){
+                if ((idmuni==localidades[i].municipio.id)){
+                    if (localidades[i].municipio.nombre!=null){
+                        contenedor.innerHTML=`Provincia: ${localidades[i].provincia.nombre} <br> Municipio: ${localidades[i].municipio.nombre}`
+                    }
+                    else{
+                        contenedor.innerHTML=`Provincia: ${localidades[i].provincia.nombre} <br> Departamento: ${localidades[i].departamento.nombre}`
+                        break
+                    }
+                }
+            }
+        }
+    })
+    }
+//
